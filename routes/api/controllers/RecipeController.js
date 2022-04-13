16fc0632cd5207d6fe996beab0ef225bb59a988b
abdelhak002 <darbeida.abdelhak@gmail.com>
2022-04-13 18:32:18 +0100

    feat update function in recipe controller

--- routes/api/controllers/RecipeController.js
+++ routes/api/controllers/RecipeController.js
@@ -1,4 +1,22 @@
 const recipes = require("../../../recipes.json");
+
+const readRecipes = (req, res) => {
+  res.status(200).json(recipes);
+};
+
+const readRecipe = (req, res) => {
+  const foundRecipe = recipes.some(
+    (recipe) => recipe.id === parseInt(req.params.id)
+  ); // returns true or false
+  if (foundRecipe) {
+    return res
+      .status(200)
+      .json(recipes.filter((recipe) => recipe.id === parseInt(req.params.id)));
+  } else {
+    return res.status(404).json({ message: "Recipe not found" });
+  }
+};
+
 const createRecipe = async (req, res) => {
   const {
     title,
@@ -21,7 +39,51 @@ const createRecipe = async (req, res) => {
     ingredients: ingredients,
     aggregateLikes: 0,
   });
-  return res.redirect("recipes");
+  return res.status(201).json(recipes);
 };
 
-module.exports = { createRecipe };
+const updateRecipe = (req, res) => {
+  const foundRecipe = recipes.some(
+    (recipe) => recipe.id === parseInt(req.params.id)
+  );
+  if (foundRecipe) {
+    const updRecipe = req.body;
+    recipes.forEach((recipe) => {
+      if (recipe.id === parseInt(req.params.id)) {
+        recipe.title = updRecipe.title ? updRecipe.title : recipe.title;
+        recipe.readyInMinutes = updRecipe.readyInMinutes
+          ? updRecipe.readyInMinutes
+          : recipe.readyInMinutes;
+        recipe.servings = updRecipe.servings
+          ? updRecipe.servings
+          : recipe.servings;
+        recipe.cuisines = updRecipe.cuisines
+          ? updRecipe.cuisines
+          : recipe.cuisines;
+        recipe.occasions = updRecipe.occasions
+          ? updRecipe.occasions
+          : recipe.occasions;
+        recipe.instructions = updRecipe.instructions
+          ? updRecipe.instructions
+          : recipe.instructions;
+        recipe.ingredients = updRecipe.ingredients
+          ? updRecipe.ingredients
+          : recipe.ingredients;
+        recipe.aggregateLikes = updRecipe.aggregateLikes
+          ? updRecipe.aggregateLikes
+          : recipe.aggregateLikes;
+        res.json({ message: "Recipe updated", recipe });
+      }
+    });
+    res.status(201).json(recipes);
+  } else {
+    res.status(400).json({ message: "Recipe not found" });
+  }
+};
+
+module.exports = {
+  readRecipes,
+  readRecipe,
+  createRecipe,
+  updateRecipe,
+};
--- routes/api/routes/recipes.js
+++ routes/api/routes/recipes.js
@@ -1,66 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const recipes = require("../../recipes");
-const recipeSchema = require("./validator/recipeValidation");
-const validator = require("./validator/validator");
-const { createRecipe } = require("./controllers/RecipeController");
+const recipes = require("../../../recipes");
+const recipeSchema = require("../validator/recipeValidation");
+const validator = require("../validator/validator");
+const {
+  readRecipes,
+  readRecipe,
+  createRecipe,
+  updateRecipe,
+} = require("../controllers/RecipeController");
 
 // GET all recipes
-router.get("/", (req, res) => {
-  res.status(200).json(recipes);
-});
+router.get("/", readRecipes);
 // GET single recipe by id
-router.get("/:id", (req, res) => {
-  const found = recipes.some((recipe) => recipe.id === parseInt(req.params.id));
-  if (found) {
-    res
-      .status(200)
-      .json(recipes.filter((recipe) => recipe.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ message: "Recipe not found" });
-  }
-});
+router.get("/:id", readRecipe);
 
 // POST recipe (create)
 router.post("/", validator(recipeSchema.createRecipe, "body"), createRecipe);
 
 // PUT recipe (update)
-router.put("/:id", (req, res) => {
-  const found = recipes.some((recipe) => recipe.id === parseInt(req.params.id));
-  if (found) {
-    const updRecipe = req.body;
-    recipes.forEach((recipe) => {
-      if (recipe.id === parseInt(req.params.id)) {
-        recipe.title = updRecipe.title ? updRecipe.title : recipe.title;
-        recipe.readyInMinutes = updRecipe.readyInMinutes
-          ? updRecipe.readyInMinutes
-          : recipe.readyInMinutes;
-        recipe.servings = updRecipe.servings
-          ? updRecipe.servings
-          : recipe.servings;
-        recipe.cuisines = updRecipe.cuisines
-          ? updRecipe.cuisines
-          : recipe.cuisines;
-        recipe.occasions = updRecipe.occasions
-          ? updRecipe.occasions
-          : recipe.occasions;
-        recipe.instructions = updRecipe.instructions
-          ? updRecipe.instructions
-          : recipe.instructions;
-        recipe.ingredients = updRecipe.ingredients
-          ? updRecipe.ingredients
-          : recipe.ingredients;
-        recipe.aggregateLikes = updRecipe.aggregateLikes
-          ? updRecipe.aggregateLikes
-          : recipe.aggregateLikes;
-        res.json({ message: "Recipe updated", recipe });
-      }
-    });
-    res.status(201).json(recipes);
-  } else {
-    res.status(400).json({ message: "Recipe not found" });
-  }
-});
+router.put(
+  "/:id",
+  /* validator(recipeSchema.updateRecipe, "body"), */ updateRecipe
+);
 
 // DELETE recipe (delete)
 router.delete("/:id", (req, res) => {
